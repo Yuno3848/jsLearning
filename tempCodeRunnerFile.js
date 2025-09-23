0cb4ade9bc19363a49adf155d60ac098767a6da2
Yuno3848 <bhandarianurag1234@gmail.com>
2025-09-23 07:53:37 +0530

    single link list

--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,6 +1,1 @@
-function sum(arr) {
-  let n = arr.length
-  return arr[n] + sum(arr[n--])
-}
-let arr = [1, 2, 3, 4, 5, 6];
-console.log(sum(arr));
+list.addAtIndex(1);
--- test.js
+++ test.js
@@ -1,22 +1,79 @@
-// function sum(n) {
-//   let isOdd = arr[n] % 2 == 1;
-//   if (n == 0) {
-//     return isOdd ? arr[n] : 0;
-//   }
+function Node(val) {
+  this.val = val;
+  this.next = null;
+}
 
-//   return isOdd ? arr[n] + sum(n - 1) : 0 + sum(n - 1);
-// }
-// let arr = [5, 2, 0, 3, 6, 7];
-// console.log(sum(arr.length - 1));
+let MyLinkList = function () {
+  this.head = null;
+  this.size = 0;
+};
 
-// function fact(n) {
-//   if (n === 1) return 1;
-//   return n * fact(n - 1);
-// }
-// let n = 5;
-// console.log(fact(n));
+MyLinkList.prototype.addAtIndex = function (val) {
+  let newNode = new Node(val);
+  newNode.next = this.head;
+  this.head = newNode;
+  this.size++;
+};
 
-let a = 0
-let b = 1
-let n = 10
-for(let i = 0;i<n;i++){}
+MyLinkList.prototype.addToTail = function (val) {
+  let newNode = new Node(val);
+  if (this.head === null) {
+    this.head = newNode;
+  } else {
+    let curr = this.head;
+    while (curr.next != null) {
+      curr = curr.next;
+    }
+    curr.next = newNode;
+  }
+  this.size++;
+};
+
+MyLinkList.prototype.addToIndex = function (val, index) {
+  if (index < 0 || index > this.size) return;
+  if (index === 0) {
+    this.addAtIndex(val);
+    return;
+  }
+  let newNode = new Node(val);
+  let curr = this.head;
+  for (let i = 0; i < index - 1; i++) {
+    curr = curr.next;
+  }
+  newNode.next = curr.next;
+  curr.next = newNode;
+  this.size++;
+};
+
+MyLinkList.prototype.deleteAtIndex = function (index) {
+  if (index < 0 || index >= this.size) return;
+  if (index === 0) {
+    this.head = this.head.next;
+    return;
+  }
+  let curr = this.head;
+  for (let i = 0; i < index - 1; i++) {
+    curr = curr.next;
+  }
+  curr.next = curr.next.next;
+  this.size--;
+};
+
+MyLinkList.prototype.print = function () {
+  let res = [];
+  let curr = this.head;
+  while (curr) {
+    res.push(curr.val);
+    curr = curr.next;
+  }
+  console.log(res.join(" -> "));
+};
+
+let list = new MyLinkList();
+list.addAtIndex(1);
+list.addToTail(2);
+list.addToTail(3);
+list.addToTail(4);
+list.addToIndex(10, 0);
+list.deleteAtIndex(1);
+list.print();
